40a3582c470ced0251157457b68d9089faf7216a
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-06-29 00:00:26 +0700

    Init

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "NixOS configuration flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/x86_64-linux";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
+
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+        hyprland.follows = "hyprland";
+      };
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          inherit system;
+
+          modules = [
+            ./hosts/default/configuration.nix
+
+            home-manager.nixosModules.home-manager {
+              home-manager.extraSpecialArgs = { inherit inputs; };
+            }
+          ];
+        };
+      };
+    };
+}
+
---
+++ modules/nixos/hyprland.nix
@@ -0,0 +1,31 @@
+{ pkgs, ... }:
+
+{
+  programs.hyprland = {
+    enable = true;
+    # Lets Hyprland launch via the Unified Wayland Session Manager (UWSM)
+    withUWSM = true;
+  };
+
+  # For controlling backlight via Light
+  programs.light.enable = true;
+
+  services.displayManager = {
+    defaultSession = "hyprland-uwsm";
+
+    sddm = {
+      # Enable SDDM as the graphical login manager
+      enable = true;
+      # Enable experimental Wayland support
+      wayland.enable = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    kitty
+  ];
+
+  # Needed for some Wayland apps to behave properly with multi-monitor setups
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+}
+
