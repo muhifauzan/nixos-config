3cf0f27aaa30c7fde6bedc02fdcb727df0aec09e
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-07-03 02:53:12 +0700

    update packages

--- modules/home-manager/packages.nix
+++ modules/home-manager/packages.nix
@@ -3,7 +3,12 @@
 {
   config = {
     programs = {
+      bat.enable = true;
+      eza.enable = true;
       firefox.enable = true;
+      fzf.enable = true;
+      ripgrep.enable = true;
+      ripgrep-all.enable = true;
       vim.enable = true;
     };
   };
