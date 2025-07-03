96b650ba8e974639950d76e4e33d4784dc27d515
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-07-03 15:42:22 +0700

    configure spotify local discovery

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -5,5 +5,10 @@
     users.users.${config.username}.packages = with pkgs; [
       spotify
     ];
+
+    networking.firewall = {
+      allowedTCPPorts = [ 57621 ];
+      allowedUDPPorts = [ 5353 ];
+    };
   };
 }
