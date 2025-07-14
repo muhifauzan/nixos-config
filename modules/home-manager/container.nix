38c59b8703f1219df92812a807c971bc6ffdfafa
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-07-14 17:38:14 +0700

    Fix container config

--- modules/home-manager/container.nix
+++ modules/home-manager/container.nix
@@ -1,17 +1,22 @@
-{ machine, ... }:
+{ ... }:
 
-let
-  inherit (machine) user;
-in
 {
   config = {
     services.podman = {
       enable = true;
+
       settings.storage = {
         storage = {
           driver = "overlay";
-          runroot = "/run/user/1000/containers";
-          graphroot = "${user.homedir}/.local/share/containers/storage";
+
+          # TODO: Figure out how to fix/coexist with bubblewrap
+          # Setting up graphroot cause bubblewrap app to use its own graphroot path
+          # which I don't know where, which change the rootless podman SUID/SGID mapping
+          # which cause error with layer something which can only be solved with
+          # podman system migrate
+          graphroot = "$XDG_DATA_HOME/containers/storage";
+
+          runroot = "$XDG_RUNTIME_DIR/containers/storage";
         };
       };
     };
--- modules/nixos/container.nix
+++ modules/nixos/container.nix
@@ -1,35 +1,22 @@
-{
-  pkgs,
-  lib,
-  machine,
-  ...
-}:
+{ machine, ... }:
 
 let
-  inherit (lib) makeBinPath;
   inherit (machine) user;
 in
 {
   config = {
     virtualisation = {
+      podman.enable = true;
       quadlet.enable = true;
-
-      podman = {
-        enable = true;
-      };
 
       containers.storage.settings = {
         storage = {
           driver = "overlay";
-          runroot = "/run/containers/storage";
           graphroot = "/var/lib/containers/storage";
+          runroot = "/run/containers/storage";
         };
       };
     };
-
-    systemd.user.extraConfig = ''
-      DefaultEnvironment="PATH=/run/current-system/sw/bin:/run/wrappers/bin:${makeBinPath [ pkgs.bash ]}"
-    '';
 
     users.users.${user.username} = {
       autoSubUidGidRange = true;
