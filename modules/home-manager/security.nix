3b9c6648cd4b1d93207f940e8a07a8dc046ddaf2
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-07-18 06:27:18 +0700

    Add home-manager secrets

--- modules/home-manager/security.nix
+++ modules/home-manager/security.nix
@@ -1,4 +1,4 @@
-{ machine, ... }:
+{ config, machine, ... }:
 
 let
   inherit (machine) user;
@@ -7,5 +7,30 @@ in
   sops = {
     defaultSopsFile = ../../.secrets/secrets.home-manager.yaml;
     age.keyFile = "${user.configHome}/age/home_manager.key";
+
+    secrets = {
+      anthropic_api_key = { };
+      openai_api_key = { };
+      onemap_username = { };
+      onemap_password = { };
+    };
   };
+
+  home.file.".config/sops-nix/load-secrets.sh" = {
+    text = ''
+      #!/usr/bin/env bash
+
+      export ANTHROPIC_API_KEY=$(cat "${config.sops.secrets.anthropic_api_key.path}" 2>/dev/null || echo "")
+      export OPENAI_API_KEY=$(cat "${config.sops.secrets.openai_api_key.path}" 2>/dev/null || echo "")
+      export ONEMAP_USERNAME=$(cat "${config.sops.secrets.onemap_username.path}" 2>/dev/null || echo "")
+      export ONEMAP_PASSWORD=$(cat "${config.sops.secrets.onemap_password.path}" 2>/dev/null || echo "")
+    '';
+
+    executable = true;
+  };
+
+  # TODO: Make zsh configurable
+  programs.zsh.initExtra = ''
+    source ${user.configHome}/sops-nix/load-secrets.sh
+  '';
 }
