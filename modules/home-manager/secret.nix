d590985a52749ae5af0804ee7ad1caf02cfa583c
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-07-22 00:50:15 +0700

    Update home-manager/secret.nix

--- modules/home-manager/secret.nix
+++ modules/home-manager/secret.nix
@@ -26,11 +26,10 @@ in
   home.file."${cfg.my.scriptHome}/sops-nix/load-secrets.sh" = {
     text = ''
       #!/usr/bin/env bash
-      # export <SOME_ENV>=$(cat $SOPS_<SOME_ENV>_PATH 2>/dev/null || echo "")
-      export SOPS_ANTHROPIC_API_KEY_PATH=${config.sops.secrets.anthropic_api_key.path}
-      export SOPS_OPENAI_API_KEY_PATH=${config.sops.secrets.openai_api_key.path}
-      export SOPS_ONEMAP_USERNAME_PATH=${config.sops.secrets.onemap_username.path}
-      export SOPS_ONEMAP_PASSWORD_PATH=${config.sops.secrets.onemap_password.path}
+      export SOPS_ANTHROPIC_API_KEY_FILE=${config.sops.secrets.anthropic_api_key.path}
+      export SOPS_OPENAI_API_KEY_FILE=${config.sops.secrets.openai_api_key.path}
+      export SOPS_ONEMAP_USERNAME_FILE=${config.sops.secrets.onemap_username.path}
+      export SOPS_ONEMAP_PASSWORD_FILE=${config.sops.secrets.onemap_password.path}
     '';
 
     executable = true;
@@ -39,5 +38,6 @@ in
   # TODO: Make zsh options configurable
   programs.zsh.initContent = mkOrder 1500 ''
     source ${cfg.my.scriptHome}/sops-nix/load-secrets.sh
+    # export <SOME_ENV>=$(cat "$SOPS_<SOME_ENV>_FILE" 2>/dev/null || echo "")
   '';
 }
