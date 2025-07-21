22cd3212946b58d852968efdb749bb3814f41ccb
Muhammad Fauzan <mfauzanmhf@gmail.com>
2025-07-22 01:35:11 +0700

    Fix wrong key paths in devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -1,19 +1,24 @@
 {
   dotenv.enable = true;
+  cachix.enable = false;
+
+  env = {
+    SOPS_CONFIG = ".secrets/.sops.yaml";
+  };
 
   scripts.sops-edit-nixos.exec = ''
-    export SOPS_AGE_KEY_FILE=$XDG_CONFIG_HOME/age/nixos.key
+    export SOPS_AGE_KEY_FILE=$HOME/.my/keys/age/nixos.key
     sops edit .secrets/secrets.nixos.yaml
   '';
 
   scripts.sops-edit-home-manager.exec = ''
-    export SOPS_AGE_KEY_FILE=$XDG_CONFIG_HOME/age/home_manager.key
+    export SOPS_AGE_KEY_FILE=$HOME/.my/keys/age/home_manager.key
     sops edit .secrets/secrets.home-manager.yaml
   '';
 
   scripts.sops-info.exec = ''
     echo "Available SOPS scripts:"
-    echo "  sops-edit-nixos        - Edit NixOS secrets"
-    echo "  sops-edit-home-manager - Edit Home Manager secrets"
+    echo "  sops-edit-nixos          - Edit NixOS secrets"
+    echo "  sops-edit-home-manager   - Edit Home Manager secrets"
   '';
 }
